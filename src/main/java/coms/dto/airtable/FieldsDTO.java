06c2955f09613590963d8d864c34ba5f82185d78
Elcolora3x <elcolora2x@gmail.com>
2025-11-14 11:19:18 -0300

    Agrega clases FieldsDTO y PictureDTO

---
+++ src/main/java/coms/dto/airtable/FieldsDTO.java
@@ -0,0 +1,59 @@
+package coms.dto.airtable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FieldsDTO {
+
+    @JsonProperty("Nombre")
+    private String nombre;
+
+    @JsonProperty("Apellido")
+    private String apellido;
+
+    @JsonProperty("Correo Moby")
+    private String correoMoby;
+
+    @JsonProperty("Foto de Perfil URL")
+    private String fotoPerfilUrl;
+
+    @JsonProperty("Provincia")
+    private String provincia;
+
+    @JsonProperty("Localidad")
+    private String localidad;
+
+    @JsonProperty("Referente")
+    private UserReferenceDTO referente;
+
+    @JsonProperty("Talent Partner")
+    private UserReferenceDTO talentPartner;
+
+    @JsonProperty("Fecha de Alta")
+    private String fechaAlta;
+
+    @JsonProperty("Proyectos")
+    private List<String> proyectos;
+
+    @JsonProperty("Direccion")
+    private String direccion;
+
+    @JsonProperty("Observaciones")
+    private String observacion;
+
+    @JsonProperty("Tecnología Actual")
+    private List<String> tecnologiaActual;
+
+    @JsonProperty("Historial de Tecnologías")
+    private List<String> historialTecnologias;
+
+}
---
+++ src/main/java/coms/dto/airtable/PictureDTO.java
@@ -0,0 +1,10 @@
+package coms.dto.airtable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PictureDTO {
+    private String url;
+}
